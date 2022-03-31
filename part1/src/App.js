0b9cf69b1c99f0c6cbb142f777dc1fd73d5a0902
makicamel <unright@gmail.com>
2022-03-31 20:29:41 +0900

    Try to useState

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,43 @@
+import { useState } from 'react'
 import './App.css';
 
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>the app is used by pressing the buttons</div>
+    )
+  } else {
+    return (
+      <div>button press history: {props.allClicks.join(' ')}</div>
+    )
   }
-  return (
-    <div>
-      <p>Hello {props.name}, you're {props.age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
 }
 
+const Button = ({ handleClick, text }) => (
+  < button onClick={handleClick} > {text}</button>
+)
+
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 }
