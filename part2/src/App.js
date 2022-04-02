41d512f16917447e91ca4e52710d42bcbcefcc9e
makicamel <unright@gmail.com>
2022-04-02 19:30:09 +0900

    Switch to show all notes or important notes only

--- part2/src/App.js
+++ part2/src/App.js
@@ -5,6 +5,8 @@ import Note from './components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
@@ -20,12 +22,20 @@ const App = (props) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => <Note key={note.id} note={note} />)}
+        {notesToShow.map(note => <Note key={note.id} note={note} />)}
       </ul>
       <form onSubmit={addNote}>
         <input
