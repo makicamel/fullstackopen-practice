ab3914a97f31a0e6e6cc676bd5fa13bbd88da3ab
makicamel <unright@gmail.com>
2022-04-04 08:22:11 +0900

    Implement POST notes

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let notes = [
   {
@@ -25,6 +26,30 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(note => note.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+  const note = {
+    id: generateId(),
+    content: body.content,
+    date: new Date(),
+    important: body.important || false
+  }
+  notes = notes.concat(note)
+  response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
