d9faa3a4ac86c12c7fc381c18b97a5dd6f1ab435
makicamel <unright@gmail.com>
2022-04-03 19:38:55 +0900

    Use express instead of http module

--- part3/index.js
+++ part3/index.js
@@ -1,10 +1,22 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+app.get('/api/notes', (request, response) => {
+  const notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2022-05-30T17:30:31.098Z",
+      important: true
+    }
+  ]
+  response.json(notes)
 })
 
 const PORT = 3004
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
