57e6fe8522abe47bfdc7fb0b6b4282a5196bed9b
makicamel <unright@gmail.com>
2022-04-07 08:14:47 +0900

    Add body to log

--- part3/index.js
+++ part3/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('body', (request) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] - :body'));
 
 let notes = [
   {
