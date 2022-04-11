90273461c2d51fed1392af1627d62a71db63dc62
makicamel <unright@gmail.com>
2022-04-11 09:55:09 +0900

    Use environment argument

--- part3/index.js
+++ part3/index.js
@@ -63,7 +63,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3004
+const PORT = process.env.PORT || 3004
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
