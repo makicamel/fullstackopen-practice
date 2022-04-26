34f2d4322cfcc986cfd8786abc3d2e7c7f8ff69a
makicamel <unright@gmail.com>
2022-04-27 08:35:15 +0900

    Set up test data

--- part4/tests/note_api.test.js
+++ part4/tests/note_api.test.js
@@ -2,6 +2,28 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const Note = require('../models/note')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    date: new Date(),
+    important: true,
+  },
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
 
 test('notes are returned as json', async () => {
   await api
@@ -12,7 +34,7 @@ test('notes are returned as json', async () => {
 
 test('there are two notes', async () => {
   const response = await api.get('/api/notes')
-  expect(response.body).toHaveLength(2)
+  expect(response.body).toHaveLength(initialNotes.length)
 })
 test('the first note is about HTTP methods', async () => {
   const response = await api.get('/api/notes')
