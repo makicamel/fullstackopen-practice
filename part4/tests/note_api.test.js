d93fa9cc84217925683bef11b031dbbf21ee4b18
makicamel <unright@gmail.com>
2022-04-28 08:02:59 +0900

    Refactor to use async/await instead of callback

--- part4/tests/note_api.test.js
+++ part4/tests/note_api.test.js
@@ -29,12 +29,16 @@ test('the first note is about HTTP methods', async () => {
   expect(response.body[0].content).toBe('HTML is easy')
 })
 
-test('a specific note is within the returned notes', async () => {
-  const response = await api.get('/api/notes')
-  const contents = response.body.map(r => r.content)
-  expect(contents).toContain(
-    'Browser can execute only JavaScript'
-  )
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+  expect(resultNote.body).toEqual(processedNoteToView)
 })
 
 test('a valid note can be added', async () => {
@@ -67,6 +71,22 @@ test('note without content is not added', async () => {
 
   const notesAtEnd = await helper.notesInDb()
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
+})
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToDelete = notesAtStart[0]
+
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204)
+
+  const notesAtEnd = await helper.notesInDb()
+  expect(notesAtEnd).toHaveLength(
+    helper.initialNotes.length - 1
+  )
+  const contents = notesAtEnd.map(r => r.content)
+  expect(contents).not.toContain(noteToDelete.content)
 })
 
 afterAll(() => {
